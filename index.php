a6a844854202bb7cd8d18c13780adc4ea3501598
Roman <roma.alf89@yandex.ru>
2019-03-04 21:07:27 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-  
+
+    $admin1 = 'admin/controller/extension/module';
+    $admin2 = 'admin/language/ru-ru/extension/module';
+    $admin3 = 'admin/view/template/extension/module';
+    $catalog1 ='catalog/controller/extension/module';
+    $catalog2 ='catalog/language/ru-ru/extension/module';
+    $catalog3 ='catalog/view/theme/default/template/extension/module';
+	
+    $all = array($admin1, $admin2, $admin3, $catalog1, $catalog2, $catalog3);
+
+    foreach ($all as $result) {
+        if (!mkdir($result, 0777, true)) {
+            die('<p> Что то не работает :(( </p>');
+        } else {
+            echo '<p> Магия! </p>';  
+        }
+    }
